66f9c12cf102b63a4aec91e3798ab38c1dd4f164
aganuza <78296644+aganuza@users.noreply.github.com>
2021-05-23 20:17:32 +0200

    Add files via upload

---
+++ funcionesParal.h
@@ -0,0 +1,4 @@
+extern void mover(struct persona *personas, struct poblacion *poblacion, int tam);
+extern int porcentaje(int tam, int n);
+extern void simulacion(struct poblacion *poblacion, struct persona *personas, double t);
+extern void Crear_Tipo (int *A, int *B, int *C, int *D, int *E, int *F, MPI_Datatype *PARAM);
